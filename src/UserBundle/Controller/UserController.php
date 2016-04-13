32d259ba1b0bc312b322a2d10ad3d5328eaf7292
CongLuat Dinh <congluat.dinh@ogilvy.com>
2016-04-13 19:22:31 +0700

    Add Forgot Password page

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -13,9 +13,28 @@ class UserController extends Controller
    */
   public function signInAction(Request $request)
   {
-    // replace this example code with whatever you need
     return $this->render('@front/user/sign_in_page.html.twig', array(
       'page_title'=>'Sign In'
+    ));
+  }
+
+  /**
+   * @Route("/sign-up", name="sign_up_page")
+   */
+  public function signUpAction(Request $request)
+  {
+    return $this->render('@front/user/sign_up_page.html.twig', array(
+      'page_title'=>'Sign Up'
+    ));
+  }
+
+  /**
+   * @Route("/forgot-password", name="forgot_password_page")
+   */
+  public function forgotPasswordAction(Request $request)
+  {
+    return $this->render('@front/user/forgot_password_page.html.twig', array(
+      'page_title'=>'Forgot Password'
     ));
   }
 }
