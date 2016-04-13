dd814ae22b295b3e7542245e8179db44473a22b8
CongLuat Dinh <congluat.dinh@ogilvy.com>
2016-04-13 16:39:43 +0700

    Init sources

---
+++ README.md
@@ -0,0 +1,4 @@
+dicolcms
+========
+
+A Symfony project created on April 13, 2016, 11:27 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@front/default/home_page.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+}
