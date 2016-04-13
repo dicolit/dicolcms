c6f47d96c97fea8bf6f30e57735e538b6564ff53
CongLuat Dinh <congluat.dinh@ogilvy.com>
2016-04-14 00:44:01 +0700

    Add pages

---
+++ src/ContactBundle/Controller/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ContactBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+  /**
+   * @Route("/contact", name="contact_page")
+   */
+  public function contactAction(Request $request)
+  {
+    return $this->render('@front/contact/contact_page.html.twig', array(
+      'page_title'=>'Contact'
+    ));
+  }
+
+}
